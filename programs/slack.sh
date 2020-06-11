c997cefd9092e0e3ff68cc0ef44522ac441d4cac
Bharath <albharath.1305@gmail.com>
2020-06-11 15:03:48 +0530

    Slack: Add version var for easy updates

--- programs/slack.sh
+++ programs/slack.sh
@@ -6,9 +6,10 @@ source "$ROOT_DIR/_utils.sh"
 
 install_slack() {
   SLACK_DEB="$SOFTWARE_DIR/slack.deb"
+  SLACK_VER="4.4.3"
 
   if [ ! -f "$SLACK_DEB" ]; then
-    wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.3-amd64.deb -O $SLACK_DEB
+    wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-$SLACK_VER-amd64.deb" -O $SLACK_DEB
   fi
 
   sudo dpkg -i $SLACK_DEB
